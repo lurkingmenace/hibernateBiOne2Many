00f75beb650894b4227ed562007c7a05c54f0ed2
John DiVirgilio <lurkingmenace@gmail.com>
2018-08-16 19:13:47 -0500

    Added delete course which deletes associated reviews

---
+++ src/com/jdivirgilio/hibernate/demo/DeleteCourseAndReviews.java
@@ -0,0 +1,54 @@
+package com.jdivirgilio.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.jdivirgilio.hibernate.demo.entity.Course;
+import com.jdivirgilio.hibernate.demo.entity.Instructor;
+import com.jdivirgilio.hibernate.demo.entity.InstructorDetail;
+import com.jdivirgilio.hibernate.demo.entity.Review;
+
+public class DeleteCourseAndReviews {
+
+	public static void main(String[] args) {
+		// Create Session Factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml") // Default name of file. Not
+																					// necessary to include here.
+																					// Must be in class path though!
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)	 
+								.addAnnotatedClass(Review.class)	 
+								.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		try {
+
+			session.beginTransaction();
+
+			// Get the course
+			int courseId = 16;
+			Course course = session.get(Course.class, courseId);
+			
+			// Print the course
+			System.out.println(course);
+			
+			// print the reviews
+			System.out.println(course.getReviews());
+			
+			// Delete the course
+			session.delete(course);
+			
+			
+			
+			session.getTransaction().commit();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+			factory.close();
+		}
+	}
+}
